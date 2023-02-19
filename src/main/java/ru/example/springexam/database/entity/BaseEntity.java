d644053adcb9831bfa6d99b0eb165536ab38f142
MaximMatveev <mockingrave@gmail.com>
2023-02-19 20:40:48 +0300

    Улучшена логика отлова изменений PostLoad/PostUpdate

--- src/main/java/ru/example/springexam/database/entity/BaseEntity.java
+++ src/main/java/ru/example/springexam/database/entity/BaseEntity.java
@@ -1,22 +1,59 @@
 package ru.example.springexam.database.entity;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PostLoad;
+import javax.persistence.PostUpdate;
+import javax.persistence.Transient;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @MappedSuperclass
-public abstract class BaseEntity {
+public abstract class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Transient
+    private BaseEntity sourceData;
+
+
+    @PostLoad
+    public void afterLoad() throws IOException, ClassNotFoundException {
+        sourceData = cloneObject();
+    }
+
+    @PostUpdate
+    public void postUpdate() {
+        log.info("Old Entity: {}", sourceData);
+        log.info("New Entity: {}", this);
+    }
+
+
+    public BaseEntity cloneObject() throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(this);
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        return (BaseEntity) ois.readObject();
+    }
 
 }
--- src/main/java/ru/example/springexam/database/entity/Employee.java
+++ src/main/java/ru/example/springexam/database/entity/Employee.java
@@ -1,14 +1,12 @@
 package ru.example.springexam.database.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.PostLoad;
-import javax.persistence.PostUpdate;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,30 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor
 //@DynamicUpdate //позволяет обновлять лишь нужные поля при генерации запроса
 @Slf4j
+@ToString
 public class Employee extends BaseEntity {
 
     @NotEmpty
     private String name;
 
     private Integer age;
-
-    @Transient
-    private Employee sourceData;
-
-    @PostLoad
-    public void afterLoad() throws CloneNotSupportedException {
-        sourceData = new Employee();
-        sourceData.setName(name);
-        sourceData.setAge(age);
-    }
-    @PostUpdate
-    public void postUpdate(){
-        if (!name.equals(sourceData.name)) printDif("name", sourceData.name, name);
-        if (!age.equals(sourceData.age)) printDif("age", sourceData.age.toString(), age.toString());
-    }
-
-    private void printDif(String name, String was, String now){
-        log.info("Changed {}: {}->{}", name, was, now);
-    }
 
 }
